1568600b73026cacd4a44ba2075708d5f951e0b4
SvisTheCoder <evilomega7@gmail.com>
2026-05-06 12:04:23 +0200

    P4 V2

--- src/exo/Partie4.java
+++ src/exo/Partie4.java
@@ -4,6 +4,7 @@ import models.Trip;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
@@ -13,34 +14,33 @@ public class Partie4 {
     ToDoubleFunction<Trip> getPrice =
             trip -> trip.price();
 
+    Function<Trip, String> getCity =
+            trip -> trip.city();
+
     Predicate<Trip> isPremiumTrip =
             trip -> trip.price() > 30 && trip.rating() > 4;
 
     public double totalRevenueSequential(List<Trip> trips) {
-        // Revenu total en stream séquentiel
         return trips.stream()
                 .mapToDouble(getPrice)
                 .sum();
     }
 
     public double totalRevenueParallel(List<Trip> trips) {
-        // Revenu total en stream parallèle
         return trips.parallelStream()
                 .mapToDouble(getPrice)
                 .sum();
     }
 
     public Map<String, Long> countByCityParallel(List<Trip> trips) {
-        // Nombre de trajets par ville en parallèle
         return trips.parallelStream()
                 .collect(Collectors.groupingBy(
-                        Trip::city,
+                        getCity,
                         Collectors.counting()
                 ));
     }
 
     public List<Trip> premiumTripsParallel(List<Trip> trips) {
-        // prix > 30 et rating > 4
         return trips.parallelStream()
                 .filter(isPremiumTrip)
                 .toList();
